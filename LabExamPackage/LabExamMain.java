dea1b2825b118ca52729540929c4c61225fa9932
shardapatil <sonyapatil265@gmail.com>
2023-07-21 11:57:03 +0530

    question1 code

--- LabExamPackage/LabExamMain.java
+++ LabExamPackage/LabExamMain.java
@@ -13,11 +13,7 @@ public class LabExamMain {
 	        String name = scanner.nextLine();
 	        System.out.println("Welcome, " + name + "!");
 	        
-	        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	        Date currentDate = new Date();
-	        System.out.println("Current Date and Time: " + sdf.format(currentDate));
-
-	        System.out.println("Welcome, " + name + "! Current Date and Time: " + sdf.format(currentDate));
+	       
 	        
 	        scanner.close();
 	}
